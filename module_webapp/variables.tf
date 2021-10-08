cc63a0d8d34547f9233d29936e099a615878f64c
amilovanovikj <andrejmilovanovic98@gmail.com>
2021-10-08 22:45:13 +0200

    Add monitoring variables to webapp module

--- module_webapp/variables.tf
+++ module_webapp/variables.tf
@@ -1,3 +1,13 @@
 variable env {
 	type = map
 }
+
+variable "appinsights_instrumentation_key" {
+  type = string
+  sensitive = true
+}
+
+variable "appinsights_connection_string" {
+  type = string
+  sensitive = true
+}
